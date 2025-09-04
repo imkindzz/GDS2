12578ab905b65b2eea8abd6f2fd99b6922e3e7c6
abz0 <143391418+abz0@users.noreply.github.com>
2025-09-04 13:18:25 +1000

    soulBody_returns_to_mainBody
    As a player, I want see the soul body return to the main body so that I know where the soul go after its summon duration ended.

--- Assets/Script/Player/PlayerSummonSoul.cs
+++ Assets/Script/Player/PlayerSummonSoul.cs
@@ -7,19 +7,28 @@ public class PlayerSummonSoul : MonoBehaviour
     [SerializeField] private GameObject soulBody; //the body of the soul summoned
     [SerializeField] private bool isSummoned = false; //whether or not the soul is summoned
     [SerializeField] private float summonDuration = 1f; //time taken for how long the soul stay summoned for
+    [SerializeField] private float returnSpeed = 1f; //the speed of when the soul returns to the main body
+
+    private PlayerSoulMovement soulMovement;
 
     private float summonTimer = 0f; //timer that tracks the time taken when the soul is summoned
+    private bool onReturn = false; //whether or not the soul returns to the main body
 
     #region Unity methods
+    void Awake()
+    {
+        soulMovement = soulBody.GetComponent<PlayerSoulMovement>();
+    }
+
     void Start()
     {
         if (isSummoned) SummonSoul();
-        else RemoveSoul();
+        else soulBody.SetActive(false);
     }
 
     void Update()
     {
-        if (!isSummoned && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
+        if (!isSummoned && !onReturn && (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             SummonSoul();
         }
@@ -30,6 +39,8 @@ public class PlayerSummonSoul : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.R) || summonTimer >= summonDuration) RemoveSoul();
         }
+
+        if (onReturn) ReturnSoul();
     }
     #endregion
 
@@ -42,14 +53,30 @@ public class PlayerSummonSoul : MonoBehaviour
 
         soulBody.transform.position = transform.position;
         soulBody.SetActive(true);
+
+        soulMovement.enabled = true;
     }
 
     //removes the soul from the game
     private void RemoveSoul()
     {
         isSummoned = false;
+        onReturn = true;
 
-        soulBody.SetActive(false);
+        soulMovement.enabled = false;
+    }
+
+    //returns the soul to the main body
+    private void ReturnSoul()
+    {
+        soulBody.transform.position = Vector2.MoveTowards(soulBody.transform.position, transform.position, returnSpeed * Time.deltaTime);
+
+        float distance = Vector2.Distance(soulBody.transform.position, transform.position);
+        if (distance < 0.05f)
+        {
+            soulBody.SetActive(false);
+            onReturn = false;
+        }
     }
     #endregion
 }
