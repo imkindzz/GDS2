471e0ab87b6776f551454d9ce42a7af9bc413180
abz0 <143391418+abz0@users.noreply.github.com>
2025-10-03 19:35:40 +1000

    Revert "adjust_for_latest_merge"
    This reverts commit 430315e43f6672b77468ae4805208cee42ba813d.

--- Assets/Script/Status/StatusBase.cs
+++ Assets/Script/Status/StatusBase.cs
@@ -111,6 +111,8 @@ public class StatusBase : MonoBehaviour
         Collider2D col = GetComponent<Collider2D>(); 
         if (col) col.enabled = false;
 
+        SoundManager.instance.PlayDeathSound();
+
         Destroy(this.gameObject, deathDelay);
     }
     #endregion
